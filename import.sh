3dd043d4d6078a6367a2387eab66778f0f083573
Antoine Villeret <antoine@makula.be>
2020-07-03 09:40:59 +0200

    use gbase64 only if it exists

--- import.sh
+++ import.sh
@@ -1,8 +1,9 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	gnubase64=gbase64
+	gnubase64="base64"
+	command -v gbase64 > /dev/null 2>&1 && gnubase64="gbase64 -w0"
 else
-	gnubase64=base64
+	gnubase64="base64 -w 0"
 fi
 
-echo -e "$(git log -100 --pretty=format:"%H*#%an*#%ae*#%at*#%s"|${gnubase64} -w 0)"
+echo -e "$(git log -100 --pretty=format:"%H*#%an*#%ae*#%at*#%s"|${gnubase64})"
 echo -e 'Double-click to select, then copy/paste it in Git History Editor'
